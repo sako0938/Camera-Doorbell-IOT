3e5369864852dadface76b3faedbb5508a7abb1e
Sam Korn <korn94sam@gmail.com>
2018-10-24 18:20:51 -0400

    exception handling and print statements

--- stream.py
+++ stream.py
@@ -28,6 +28,7 @@ def get_frame():
     i=1
     while True:
         stringData = cam_q.get()
+        print("size of queue: %d" % cam_q.qsize() )
         yield (b'--frame\r\n'
             b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
         i+=1
@@ -45,12 +46,16 @@ def image_thread():
 
     camera=cv2.VideoCapture(camera_port) #this makes a web cam object
     while True:
-        print("Snapping a photo!")
-        retval, im = camera.read()
-        imgencode=cv2.imencode('.jpg',im)[1]
-        stringData=imgencode.tostring()
-        cam_q.put(stringData,block=True)
-        sleep(0.25)
+        try:
+            print("Snapping a photo!")
+            retval, im = camera.read()
+            imgencode=cv2.imencode('.jpg',im)[1]
+            stringData=imgencode.tostring()
+            cam_q.put(stringData,block=True)
+            sleep(0.25)
+        except Exception as e:
+            print("Exception taking photo: %s" % str(e) )
+            sleep(0.5)
 
 
 if __name__ == '__main__':
